43e9a7d3b1c5a3a46712d35fa5ba73fab1d684e3
zootedb0t <05kraken@proton.me>
2022-12-03 19:36:54 +0530

    random stuff

--- scripts/.local/bin/screenshot.sh
+++ scripts/.local/bin/screenshot.sh
@@ -7,10 +7,30 @@ geometry="$(xrandr | head -n1 | cut -d',' -f2 | tr -d '[:blank:],current')"
 dir="$(xdg-user-dir PICTURES)/Screenshots"
 file="Screenshot_${time}_${geometry}.png"
 
+if [[ ! -d "$dir" ]]; then
+	mkdir -p "$dir"
+fi
+
+# Options for screenshots
+shotnow=" Take Screenshot"
+shotarea=" Take area screenshot"
+timer_five="羽 Take screenshot in 5 sec"
+timer_ten="福 Take screenshot in 10 sec"
+
+# Choose option from rofi menu
+selected_option=$( echo "$shotnow
+$shotarea
+$timer_five
+$timer_ten
+" | dmenu\
+  -i\
+  -p "Choose one"\
+)
+
 # notify
 notify_user () {
 	if [[ -e "$dir/$file" ]]; then
-		dunstify -u low --replace=699 -i "$dir/$file" "Screenshot Saved"
+		dunstify -u low --replace=699 -i "$dir/$file" "Screenshot Saved" "$file"
 	else
 		dunstify -u low --replace=699 -i "$dir/$file" "Screenshot Deleted."
 	fi
@@ -28,8 +48,8 @@ countdown () {
 shotnow () {
 	cd "${dir}" && maim -u -f png "$file"
 	notify_user
-# TODO clipboard not working
-# xclip -selection clipboard -t image/png "$dir/$file"
+  # TODO clipboard not working
+  xclip -selection c -t image/png "$dir/$file.png"
 }
 
 shot5 () {
@@ -54,41 +74,6 @@ shotarea () {
 	notify_user
 }
 
-if [[ ! -d "$dir" ]]; then
-	mkdir -p "$dir"
-fi
-
-# if [[ "$1" == "--now" ]]; then
-# 	shotnow
-# elif [[ "$1" == "--in5" ]]; then
-# 	shot5
-# elif [[ "$1" == "--in10" ]]; then
-# 	shot10
-# elif [[ "$1" == "--win" ]]; then
-# 	shotwin
-# elif [[ "$1" == "--area" ]]; then
-# 	shotarea
-# else
-# 	echo -e "Available Options : --now --in5 --in10 --win --area"
-# fi
-
-# Options for screenshots
-shotnow=" Take Screenshot"
-shotarea=" Take area screenshot"
-timer_five="羽 Take screenshot in 5 sec"
-timer_ten="福 Take screenshot in 10 sec"
-
-# Choose option from rofi menu
-selected_option=$( echo "$shotnow
-$shotarea
-$timer_five
-$timer_ten
-" | rofi -dmenu\
-  -i\
-  -p "Choose one"\
-  -selected-row 0\
-)
-
 if [ "$selected_option" == "$shotnow" ]; then
   shotnow
 elif [ "$selected_option" == "$shotarea" ]; then
@@ -100,5 +85,3 @@ elif [ "$selected_option" == "$timer_ten" ]; then
 else
   echo "No Match"
 fi
-
-exit 0
