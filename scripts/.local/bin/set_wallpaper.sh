0a074500c4c5b0306759bb5c427f8175da8a973c
stoney <05kraken@proton.me>
2023-01-08 17:56:01 +0530

    update wallpaper script

--- scripts/.local/bin/set_wallpaper.sh
+++ scripts/.local/bin/set_wallpaper.sh
@@ -3,13 +3,15 @@
 wal_dir=/home/stoney/Pictures/wallpapers
 
 if [ -z "$1" ]; then
-  wall="$(find "$wal_dir" -type f -name "*.jpg" -o -name "*.png" | shuf -n1)"
+	wall="$(find "$wal_dir" -type f -name "*.jpg" -o -name "*.png" | shuf -n1)"
 else
-  wall="$1"
+	wall="$1"
 fi
 
 convert "$wall" ~/.local/share/bg.jpg
 xwallpaper --zoom ~/.local/share/bg.jpg
 wal -c
 wal -i ~/.local/share/bg.jpg
-xdotool key super+F5
+if pgrep -x "dwm" >/dev/null; then
+	xdotool key super+F5
+fi
