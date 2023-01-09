d0299dd4e22776131bb2922bcceda891d37b045a
stoney <05kraken@proton.me>
2023-01-09 22:41:44 +0530

    update font and add battery notification

---
+++ scripts/.local/bin/battery.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+battery_perc="$(cat /sys/class/power_supply/BAT0/capacity)"
+
+battery_notify() {
+	if [ "$battery_perc" -lt 20 ]; then
+		dunstify -t 2000 -u critical --replace=100 "Low Battry!!" "Please plugin"
+	fi
+}
+
+while true; do
+	sleep 600 && battery_notify
+done
