b15164920360b1ea692be1d33549f7c1dff8543e
stoney <05kraken@proton.me>
2023-01-19 20:30:17 +0530

    fix screenschot menu being captured when taking screenshot

--- scripts/.local/bin/screenshot.sh
+++ scripts/.local/bin/screenshot.sh
@@ -8,80 +8,63 @@ dir="$(xdg-user-dir PICTURES)/Screenshots"
 file="Screenshot_${time}_${geometry}.png"
 
 if [[ ! -d "$dir" ]]; then
-  mkdir -p "$dir"
+	mkdir -p "$dir"
 fi
 
 # Options for screenshots
 shotnow=" Take Screenshot"
 shotarea=" Take area screenshot"
-timer_five="羽 Take screenshot in 5 sec"
-timer_ten="福 Take screenshot in 10 sec"
+shotwin=" Take current screenshot"
 
-# Choose option from rofi menu
-selected_option=$( echo "$shotnow
+# Choose option from dmenu
+selected_option=$(
+	echo "$shotnow
 $shotarea
-$timer_five
-$timer_ten
-" | dmenu\
-  -i\
-  -p "Choose one"\
+$shotwin
+" | dmenu -i -p "Choose one"
 )
 
 # notify
-notify_user () {
-  if [[ -e "$dir/$file" ]]; then
-    dunstify -u low --replace=699 -i "$dir/$file" "Screenshot Saved" "$file"
-  else
-    dunstify -u low --replace=699 -i "$dir/$file" "Screenshot Deleted."
-  fi
+notify_user() {
+	if [[ -e "$dir/$file" ]]; then
+		dunstify -u low --replace=699 -i "$dir/$file" "Screenshot Saved" "$file"
+	else
+		dunstify -u low --replace=699 -i "$dir/$file" "Screenshot Deleted."
+	fi
 }
 
 # countdown
-countdown () {
-  for sec in $(seq "$1" -1 1); do
-    dunstify -t 1000 --replace=699 "Taking shot in : $sec"
-    sleep 1
-  done
+countdown() {
+	for sec in $(seq "$1" -1 1); do
+		dunstify -t 1000 --replace=699 "Taking shot in : $sec"
+		sleep 1
+	done
 }
 
 # take shots
-shotnow () {
-  cd "${dir}" && maim -u -f png "$file"
-  notify_user
-  # TODO clipboard not working
-  # xclip -selection clipboard -t image/png -i "$file"
+shotnow() {
+	cd "${dir}" && sleep 1; maim -u -f png "$file"
+	notify_user
+	# TODO clipboard not working
+	# xclip -selection clipboard -t image/png -i "$file"
 }
 
-shot5 () {
-  countdown '5'
-  sleep 1 && cd "${dir}" && maim -u -f png "$file"
-  notify_user
+shotwin() {
+	cd "${dir}" && maim -u -f png -i "$(xdotool getactivewindow)" "$file"
+	notify_user
 }
 
-shot10 () {
-  countdown '10'
-  sleep 1 && cd "${dir}" && maim -u -f png "$file"
-  notify_user
-}
-
-# shotwin () {
-#   cd ${dir} && maim -u -f png -i `xdotool getactivewindow` "$file"
-#   notify_user
-# }
-
-shotarea () {
-  cd "${dir}" && maim -u -f png -s -b 2 -c 0.35,0.55,0.85,0.25 -l "$file"
-  notify_user
+shotarea() {
+	cd "${dir}" && maim -u -f png -s -b 2 -c 0.35,0.55,0.85,0.25 -l "$file"
+	notify_user
 }
 
 if [ "$selected_option" == "$shotnow" ]; then
-  shotnow;
+	shotnow
 elif [ "$selected_option" == "$shotarea" ]; then
-  shotarea
-elif [ "$selected_option" == "$timer_five" ]; then
-  shot5
-elif [ "$selected_option" == "$timer_ten" ]; then
-  shot10
+	shotarea
+elif [ "$selected_option" == "$shotwin" ]; then
+	shotwin
 else
-  echo "No Match"
+	echo "No Match"
 fi
