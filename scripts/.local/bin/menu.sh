5cde971b4f0d34622ca5215590c225ca4b0cf232
stoney <05kraken@proton.me>
2023-02-21 06:42:24 +0530

    add script for dwm menu

---
+++ scripts/.local/bin/menu.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Dependency xmenu, xclickroot. Shows a menu on right-click.
+# Use tabs. If expandtab is set true, disable it in vim. Space between line gives separator.
+
+cat <<EOF | xmenu | sh &
+󰆧 Launch Dmenu 	dmenu_run
+
+󱐋 Applications
+	   Browser	firefox-nightly
+	  Terminal	st
+	  Files	thunar
+	  Pdf Reader 	zathura
+
+ RSS Feed 	st -e newsboat
+
+ Config
+	󰆧 System
+		 Edit dwm config 	st -e nvim ~/.local/src/suckless/dwm/config.def.h
+		 Edit picom config 	st -e nvim ~/.config/picom/picom.conf
+		 Edit st config 	st -e nvim ~/.local/src/suckless/orcst/config.def.h
+		 Edit menu 	st -e nvim ~/.local/bin/menu.sh
+		 Xresources 	st -e nvim $HOME/.Xresources
+	󰲋 Application
+		 Volume control 	pavucontrol
+		 Newsboat 	st -e nvim ~/.config/newsboat/config
+		 Nvim 	st -e nvim ~/.config/nvim/init.lua
+		󰎟 Dunst 	st -e nvim ~/.cache/wal/dunstrc
+
+ Theming
+	󰉦 Lxappearance 	lxappearance
+	 Wallpapers 	st -e nsxiv ~/Pictures/wallpapers/
+
+ Monitor
+	󱜙 Btop 	st -e btop
+	 Update System 	st -e sudo pacman -Syu
+
+⏻ Power off 	systemctl poweroff
+ Reboot 	systemctl reboot
+󰍂 Log out 	loginctl terminate-user "$(whoami)"
+EOF
