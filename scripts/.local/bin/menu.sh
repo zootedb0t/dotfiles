ff025a6960e1ec6943e83defb8857af02f9fe494
stoney <05kraken@proton.me>
2023-02-26 17:06:11 +0530

    new start

--- scripts/.local/bin/menu.sh
+++ scripts/.local/bin/menu.sh
@@ -5,24 +5,29 @@
 
 cat <<EOF | xmenu | sh &
 󰆧 Launch Dmenu 	dmenu_run
+ Launch Terminal	st
 
 󱐋 Applications
-	   Browser	firefox-nightly
-	  Terminal	st
-	  Files	thunar
-	  Pdf Reader 	zathura
+	󰖟  Browser
+		 Firefox 	firefox-nightly
+		 Chromium 	chromium
+	  Utilities
+		 Files	thunar
+		 Pdf Reader 	zathura
+		 Proton Vpn 	protonvpn
+	󰇰 Thunderbird 	thunderbird
 
  RSS Feed 	st -e newsboat
 
  Config
 	󰆧 System
+		 Volume control 	pavucontrol
 		 Edit dwm config 	st -e nvim ~/.local/src/suckless/dwm/config.def.h
 		 Edit picom config 	st -e nvim ~/.config/picom/picom.conf
 		 Edit st config 	st -e nvim ~/.local/src/suckless/orcst/config.def.h
 		 Edit menu 	st -e nvim ~/.local/bin/menu.sh
 		 Xresources 	st -e nvim $HOME/.Xresources
 	󰲋 Application
-		 Volume control 	pavucontrol
 		 Newsboat 	st -e nvim ~/.config/newsboat/config
 		 Nvim 	st -e nvim ~/.config/nvim/init.lua
 		󰎟 Dunst 	st -e nvim ~/.cache/wal/dunstrc
@@ -30,6 +35,11 @@ cat <<EOF | xmenu | sh &
  Theming
 	󰉦 Lxappearance 	lxappearance
 	 Wallpapers 	st -e nsxiv ~/Pictures/wallpapers/
+
+ Misc 
+	Colors Script
+		Cbonsai 	st -e cbonsai
+	Cava 	st -e cava
 
  Monitor
 	󱜙 Btop 	st -e btop
