b35ea104e1e24ff420c52fa0872e10d80bd94fff
zootedb0t <05kraken@proton.me>
2023-04-16 23:25:32 +0530

    update picom, pywal template, volumecontrol script

--- scripts/.local/bin/volumeControl.sh
+++ scripts/.local/bin/volumeControl.sh
@@ -5,33 +5,16 @@
 # $ ./volumeControl.sh down
 # $ ./volumeControl.sh mute
 
-DIR="$HOME/.config/dunst"
-
 function get_volume {
 	amixer sget Master | grep 'Right:' | awk -F'[' '{print $2}' | awk -F'%' '{print $1}'
 	# wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk -F' ' '{print $2}' | awk -F'.' '{print $2}'
 }
 
+# Send the notification
 function send_notification {
 	volume=$(get_volume)
-	# bar=$(seq -s "─" 0 $((volume / 5)) | sed 's/[0-9]//g')
-	if [ "$volume" -lt "10" ]; then
-		icon_name="$DIR/icons/volume-10.png"
-	else
-		if [ "$volume" -lt "30" ]; then
-			icon_name="$DIR/icons/volume-10.png"
-		else
-			if [ "$volume" -lt "70" ]; then
-				icon_name="$DIR/icons/volume-30.png"
-			else
-				icon_name="$DIR/icons/volume-70.png"
-			fi
-		fi
-	fi
-	# fi
-	# Send the notification
-	dunstify "Volume $volume% " -i "$icon_name" -r 5555 -u normal -h int:value:$((volume))
-	# dunstify "Volume $volume% " -r 5555 -u normal -h int:value:$((volume))
+	dunstify "Volume $volume% " -r 5555 -u normal -h int:value:$((volume))
+	# dunstify "Volume $volume% " -r 5555 -u normal
 }
 
 case $1 in
@@ -55,8 +38,7 @@ mute)
 	wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
 	is_mute="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk -F' ' '{print $3}' | awk -F'[][]' '{print $2}')"
 	if [ "$is_mute" == "MUTED" ]; then
-		icon_name="$DIR/icons/mute.png"
-		dunstify "Mute" -i "$icon_name" -r 5555 -u normal -h int:value:0
+		dunstify "Mute" -r 5555 -u normal -h int:value:0
 	else
 		send_notification
 	fi
